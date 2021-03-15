34451457fed2cce4d547d31d614535c7b7711b70
Mauri Gaggero <mauricio.gaggero@hotmail.com>
2021-03-15 17:05:43 -0300

    add hubs

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string usuario, string mensaje, DateTime hora)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", usuario, mensaje, hora);
+        }
+    }
+}
